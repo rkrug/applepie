d7b494f6f8cebca4c2fd7d518ad99807df18a74a
Rainer M. Krug <Rainer@krugs.de>
2021-07-07 11:29:34 +0200

    Demo Skip and others

--- tests/testthat/test-packages.R
+++ tests/testthat/test-packages.R
@@ -1,4 +1,30 @@
 test_that("get_packages() works", {
-  pkgs <- get_packages("maelle")
+  vcr::use_cassette("packages", {
+    pkgs <- get_packages("LEEF-UZH")
+  })
+  expect_vector(pkgs, character())
+})
+
+
+test_that("get_packages() error", {
+  vcr::skip_if_vcr_off()
+  vcr::use_cassette("packages-error", {
+    expect_error(get_packages("maelle"), "take off")
+  })
+})
+
+test_that("get_packages() retries", {
+  vcr::skip_if_vcr_off()
+  vcr::use_cassette("packages-retry", {
+    expect_message(pkgs <- get_packages("maelle"), "try")
+  })
+  expect_vector(pkgs, character())
+})
+
+test_that("get_packages() works", {
+  skip_if_not_beyonce()
+  vcr::use_cassette("blabla", {
+    pkgs <- get_packages("maelle")
+  })
   expect_vector(pkgs, character())
 })
---
+++ tests/testthat/test-packages2.R
@@ -0,0 +1,11 @@
+test_that("get_packages2() works", {
+  vcr::use_cassette("packages2", {
+    pkgs <- get_packages2("maelle")
+  })
+  expect_vector(pkgs, character())
+})
+
+test_that("get_packages2 errors well", {
+  withr::local_envvar("SECRET_PLANET_TOKEN" = "")
+  expect_snapshot_error(get_packages2("maelle"))
+})
